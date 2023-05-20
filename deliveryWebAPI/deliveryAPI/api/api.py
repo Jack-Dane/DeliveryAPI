501f7d73e44cf561cbc43a577c02238ef473df55
Jack-Dane <jackd98.jd@gmail.com>
2023-05-20 22:44:03 +0100

    Restrict CORS Origin to front-end application

--- deliveryWebAPI/deliveryAPI/api/api.py
+++ deliveryWebAPI/deliveryAPI/api/api.py
@@ -11,8 +11,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
